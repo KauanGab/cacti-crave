118b7c936f764a3017c26f813f9c748b48d99439
KauaGab <auansozi@gmail.com>
2023-07-12 22:38:38 -0300

    changing nav bar

--- lib/views/content/content_page.dart
+++ lib/views/content/content_page.dart
@@ -1,6 +1,7 @@
 import 'package:cacti_crave/views/content/inicial_page.dart';
 import 'package:cacti_crave/views/content/profile_page.dart';
 import 'package:cacti_crave/views/content/search_page.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class ContentPage extends StatefulWidget {
@@ -34,23 +35,20 @@ class _ContentPageState extends State<ContentPage> {
         children: [InicialPage(), SearchPage(), ProfilePage()],
         onPageChanged: setPaginaAtual,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        unselectedItemColor: Color(0xFF474747),
-        selectedItemColor: Color(0xFF47B67E),
-        currentIndex: paginaAtual,
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.white,
+        buttonBackgroundColor: Color(0xFF47B67E),
+        color: Color.fromARGB(255, 240, 240, 240),
         items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined), label: 'Início'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.search_outlined), label: 'Buscar'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person_4_outlined), label: 'Perfil')
+          Icon(Icons.home_outlined),
+          Icon(Icons.search_outlined),
+          Icon(Icons.person_4_outlined)
         ],
         onTap: (pagina) {
           pc.animateToPage(
             pagina,
-            duration: Duration(milliseconds: 400),
-            curve: Curves.linear,
+            duration: Duration(milliseconds: 450),
+            curve: Curves.ease,
           );
         },
       ),
